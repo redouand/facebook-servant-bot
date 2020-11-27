1aacfe407a6d7ab8153b9489ad877be4f0263551
redwan <theomurf47@gmail.com>
2020-11-27 15:33:34 +0100

    set headless to true

--- messenger.js
+++ messenger.js
@@ -15,7 +15,7 @@ let browser = null
 let page = null;
 async function launch() {
     try {
-        browser = await puppeteer.launch({ headless: false, defaultViewport: null })
+        browser = await puppeteer.launch({ headless: true, defaultViewport: null })
         page = await browser.newPage()
     } catch (error) {
         console.log(error.message);
